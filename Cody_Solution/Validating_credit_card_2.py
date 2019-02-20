afe8b49cec20667f6b960f4485bbfb9ccb8456d2
priyankafnu05 <47825013+priyankafnu05@users.noreply.github.com>
2019-02-20 12:41:31 -0600

    Update Validating_credit_card_2.py

--- Cody_Solution/Validating_credit_card_2.py
+++ Cody_Solution/Validating_credit_card_2.py
@@ -1,9 +1,12 @@
 import re
 PATTERN='^([456][0-9]{3})-?([0-9]{4})-?([0-9]{4})-?([0-9]{4})$'
+
 def valid_card_number(sequence):
     match = re.match(PATTERN,sequence)
+    
     if match == None:
         return False
+    
     for group in match.groups:
         if group[0] * 4 == group:
             return False
